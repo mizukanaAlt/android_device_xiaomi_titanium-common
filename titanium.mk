7fba046cb668e982a6c66d121faa9d59159cc340
mizukanaAlt <rina301087@gmail.com>
2025-10-21 23:29:22 +0000

    titanium-common: biometrics: rebrand to xiaomi_titanium

--- titanium.mk
+++ titanium.mk
@@ -17,7 +17,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_vince
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_titanium
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
